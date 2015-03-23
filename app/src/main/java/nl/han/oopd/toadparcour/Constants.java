f5b293beaca49690a0db0559c23b1b36201d6656
imacbest <thomas.erin999@gmail.com>
2015-03-23 14:44:22 +0100

    changes

---
+++ app/src/main/java/nl/han/oopd/toadparcour/Constants.java
@@ -0,0 +1,11 @@
+package nl.han.oopd.toadparcour;
+
+/**
+ * Constants
+ * @author Thomas
+ */
+public interface Constants {
+    public static final double EASY = 1.5;
+    public static final double MEDIUM = 1;
+    public static final double HARD = .5;
+}
