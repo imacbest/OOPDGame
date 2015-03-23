403ee8e5c20d92e042532bf89f6ebdb49aa6a4a6
imacbest <thomas.erin999@gmail.com>
2015-03-23 11:54:20 +0100

    added classes

---
+++ app/src/main/java/nl/han/oopd/toadparcour/ItemController.java
@@ -0,0 +1,14 @@
+package nl.han.oopd.toadparcour;
+
+import android.gameengine.icadroids.alarms.IAlarm;
+
+/**
+ * Controles the items
+ */
+public class ItemController implements IAlarm {
+
+    @Override
+    public void triggerAlarm(int alarmID) {
+
+    }
+}
---
+++ app/src/main/java/nl/han/oopd/toadparcour/MonsterController.java
@@ -0,0 +1,15 @@
+package nl.han.oopd.toadparcour;
+
+import android.gameengine.icadroids.alarms.IAlarm;
+
+/**
+ * controles the alarms for the monsters
+ */
+public class MonsterController implements IAlarm {
+
+
+    @Override
+    public void triggerAlarm(int alarmID) {
+
+    }
+}
