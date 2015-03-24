fa812b9b6c8b68cd1f5b8473ae28998cf49235df
imacbest <thomas.erin999@gmail.com>
2015-03-24 11:25:26 +0100

    dunno

--- app/src/main/java/nl/han/oopd/toadparcour/Coin.java
+++ app/src/main/java/nl/han/oopd/toadparcour/Coin.java
@@ -3,12 +3,13 @@ package nl.han.oopd.toadparcour;
 import android.gameengine.icadroids.input.OnScreenButton;
 import android.gameengine.icadroids.input.OnScreenButtons;
 import android.gameengine.icadroids.objects.MoveableGameObject;
+import android.util.Log;
 
 /**
  * Creating the coins whicht the main character can pick up en collect points
  */
 public class Coin extends Items {
-
+    private int i = 0;
     public Coin() {
         //super(target, range, speed);
         setSprite("coin", 4);
@@ -19,7 +20,6 @@ public class Coin extends Items {
     public void update(){
         super.update();
         this.turn();
-        //this.move();
     }
 
     //@Override
@@ -29,12 +29,7 @@ public class Coin extends Items {
 
 
     public void turn() {
-        if(OnScreenButtons.buttonX) {
-            setFrameNumber(2);
-        } else {
-            setFrameNumber(1);
-
-        }
+        setFrameNumber(i % 4);
     }
 }
 
