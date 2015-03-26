21f43716a8590733d82df3a7a5fa71c860ef027e
imacbest <thomas.erin999@gmail.com>
2015-03-26 14:05:46 +0100

    javadoc

--- app/src/main/java/nl/han/oopd/toadparcour/Toad.java
+++ app/src/main/java/nl/han/oopd/toadparcour/Toad.java
@@ -37,7 +37,7 @@ public class Toad extends GravatiyGameObject implements ICollision {
      */
     private int score = 0;
 
-    // ToDo: max wat doen deze variabelen?
+    // ToDo: max wat doen deze variabelen? Javadoc toevoeg :)
     private int start = 0;
     private double kracht = 6.5;
     private boolean jump = false;
